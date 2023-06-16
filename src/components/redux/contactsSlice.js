74f6eb3af2558513b426e3856a6f368e54d48977
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-06-16 20:27:41 +0300

    new commit

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const contactsInitialState = [
+const contactsInitialState = { items: [
   {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-];
+] };
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: contactsInitialState,
   reducers: {
     addContact(state, action) {
-      state.push(action.payload);
+      state.items.push(action.payload);
     },
     prepare({ id, name, number }) {
       return {
@@ -22,8 +22,8 @@ const contactsSlice = createSlice({
       };
     },
     deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.items.findIndex(contact => contact.id === action.payload);
+      state.items.splice(index, 1);
     },
   },
 })
@@ -32,4 +32,4 @@ export const { addContact, deleteContact } = contactsSlice.actions; // actions
 
 export const contactsReducer = contactsSlice.reducer; // reducers
 
-export const getContacts = state => state.contacts;
+export const getContacts = state => state.contacts.items;
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,21 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
-/* import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 const persistConfig = {
   key: 'contacts',
   storage,
-}; */
+};
 
-/* const persistedReducer = persistReducer(persistConfig, contactsReducer) */
+const persistedReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
   reducer: {
-    contacts: /* persistedReducer */ contactsReducer,
+    contacts: persistedReducer /* contactsReducer */,
     filter: filterReducer,
   }
 });
 
-/* export const persistor = persistStore(store); */
+export const persistor = persistStore(store);
